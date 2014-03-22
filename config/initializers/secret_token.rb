416124a8a7b130f857e67fdd7407afb26fba9a3f
Maxime Prades <mprades@zendesk.com>
2014-03-22 23:43:10 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Discoverzat::Application.config.secret_key_base = '9be037136de1b7e3fd69063873037766815f9018188c5a39cac15a5631fb7677073ec0909828da3fb7f282a9aa765c6872da2f6eec494cdd4728a3dba6475349'
